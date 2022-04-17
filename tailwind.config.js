0570dcc6d4c947c59aceb3bf5198f5ddaff9f283
akuutauf <taufikhidayat09121@gmail.com>
2022-04-17 10:38:52 +0700

    Upload Portfolio Website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#6366f1",
+        secondary: "#64748b",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
