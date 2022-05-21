bc5e1b3ff90e39eb37a8d2e6dbfc6a9d13ebc1da
akuutauf <taufikhidayat09121@gmail.com>
2022-05-21 14:06:59 +0700

    2D Model and 3D Model

--- dist/js/canvas_2d.js
+++ dist/js/canvas_2d.js
@@ -91,23 +91,73 @@ function scaleFish() {
   ctx.fillStyle = blueFin;
   ctx.fill();
 
-  // sisik ikan
-  // ctx.restore();
-  // ctx.beginPath();
-  // ctx.arc(210, 185, 8, 0, 1 * Math.PI, false);
-  // ctx.strokeStyle = "#000000";
-  // ctx.stroke();
-  // // hiasan bulu owl atas kiri
-  // ctx.beginPath();
-  // ctx.arc(190, 185, 8, 0, 1 * Math.PI, false);
-  // ctx.strokeStyle = "#000000";
-  // ctx.stroke();
-  // // hiasan bulu owl bawah tengah
-  // ctx.beginPath();
-  // ctx.arc(200, 198, 8, 0, 1 * Math.PI, false);
-  // ctx.strokeStyle = "#000000";
-  // ctx.stroke();
+  // sisik ikan 1
+  ctx.beginPath();
+  ctx.arc(170, 280, 14, 2, 1.6 * Math.PI, true);
+  ctx.strokeStyle = whiteEye;
+  ctx.lineWidth = 3;
+  ctx.stroke();
+
+  // sisik ikan 2
+  ctx.beginPath();
+  ctx.arc(158, 290, 8, 2, 1.6 * Math.PI, true);
+  ctx.strokeStyle = whiteEye;
+  ctx.lineWidth = 3;
+  ctx.stroke();
+
+  // sisik ikan 3
+  ctx.beginPath();
+  ctx.arc(165, 265, 10, 2, 1.6 * Math.PI, true);
+  ctx.strokeStyle = whiteEye;
+  ctx.lineWidth = 3;
+  ctx.stroke();
 }
+
+function fieldText() {
+  // field text 1
+  let fieldUtama1 = [
+    { x: 160, y: 380 },
+    { x: 200, y: 380 },
+  ];
+
+  polygon(imageDataSaya, fieldUtama1, 0, 0, 0);
+
+  // proses transformasi komposit
+  let fieldHasil1 = translasiArray(fieldUtama1, { x: 300, y: 0 });
+  polygon(imageDataSaya, fieldHasil1, 0, 0, 0);
+
+  ctx.putImageData(imageDataSaya, 0, 0);
+
+  // field text 1
+  let fieldUtama2 = [
+    { x: 210, y: 380 },
+    { x: 250, y: 380 },
+  ];
+
+  polygon(imageDataSaya, fieldUtama2, 0, 0, 0);
+
+  // proses transformasi komposit
+  let fieldHasil2 = translasiArray(fieldUtama2, { x: 300, y: 0 });
+  polygon(imageDataSaya, fieldHasil2, 0, 0, 0);
+
+  ctx.putImageData(imageDataSaya, 0, 0);
+}
+
+function textObject() {
+  // main text
+  ctx.font = "bolder 18pt verdana";
+  ctx.textAlign = "left";
+  ctx.fillStyle = "#F70AB2";
+  ctx.fillText("F", 75, 370, 320, 100);
+  ctx.fillText("I", 125, 370, 320, 100);
+  ctx.fillText("S", 170, 370, 320, 100);
+  ctx.fillText("H", 220, 370, 320, 100);
+}
+
+// pemanggilan fungsi object native
+ctx.restore();
+fieldText();
+textObject();
 
 // pemanggilan fungsi object 2d
 finFish();
--- dist/js/script_3d.js
+++ dist/js/script_3d.js
@@ -1,10 +1,10 @@
-// import library Three JS Online
+// import library three js for online use
 // import * as THREE from "https://cdn.skypack.dev/three@0.122.0/build/three.module.js";
 
-// Program Main Three JS Model 3d (belum fix)
+// Program Main Three JS
 
 // variabel untuk ukuran dunia 3d
-let width = window.innerWidth - 33;
+let width = window.innerWidth;
 let height = window.innerHeight;
 
 // scene
@@ -19,55 +19,12 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
-// Geometri mars
+// Geometri Earth
 const geo = new THREE.SphereGeometry(1.1, 20, 20);
 
-// Geometry asteroid (kanan)
-// const geo2 = new THREE.BufferGeometry();
-const geo2 = new THREE.IcosahedronBufferGeometry(1.1, 0);
-
-// Geometry asteroid (atas)
-const geo3 = new THREE.DodecahedronBufferGeometry(1.1, 0);
-
-// Custom geometry asteroid (bawah)
-const geo4 = new THREE.BufferGeometry();
-
-// vertex custom geometry asteroid (bawah)
-let vertices = new Float32Array([
-  // vertex 0 (depan)
-  0, 0, 1,
-  // vertex 1 (kanan dalam)
-  1, 0, -1,
-  // vertex 2 (kiri dalam)
-  -1, 0, -1,
-  // vertex 3 (atas)
-  0, 1, 0,
-]);
-
-let uvs = new Float32Array([
-  0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
-
-  1.0, 1.0, 0.0, 1.0, 0.0, 0.0,
-]);
-
-geo4.setIndex([
-  1,
-  3,
-  2, //kiri
-  0,
-  3,
-  1, //kanan
-  0,
-  1,
-  2, //depan
-  0,
-  2,
-  3, //bawah
-]);
-
 // planet textures
-const asteroid = new THREE.TextureLoader().load("https://i.postimg.cc/L6FXTf41/moon.jpg");
-const mars = new THREE.TextureLoader().load("https://i.postimg.cc/SQMRCV44/mars.jpg");
+const neptuneFragment = new THREE.TextureLoader().load("https://i.postimg.cc/SsGh3m8T/neptune.jpg");
+const earth = new THREE.TextureLoader().load("https://i.postimg.cc/D0yTKRP2/earth.jpg");
 
 // MeshLambertMaterial
 // pencahayaan 1
@@ -76,71 +33,26 @@ light1.position.set(-15, 20, 10);
 scene.add(light1);
 
 // pencahayaan 2
-let light2 = new THREE.PointLight(0xffffff, 1.1);
+let light2 = new THREE.PointLight(0xffffff, 0.15);
 light2.position.set(10, -10, 10);
 scene.add(light2);
 
-// material texture geometry mars
+// material texture geometry earth
 const material = new THREE.MeshPhongMaterial({
-  map: mars,
+  map: earth,
   shininess: 500,
-  bumpMap: mars,
-  bumpScale: 0.01,
+  bumpMap: earth,
+  bumpScale: 0.015,
 });
 
 let mesh = new THREE.Mesh(geo, material);
-mesh.position.set(-1.5, 0, -1);
+mesh.position.set(0, 0, 0);
 scene.add(mesh);
-
-// material geometri asteroid (kanan)
-const material2 = new THREE.MeshPhongMaterial({
-  map: asteroid,
-  shininess: 500,
-  bumpMap: asteroid,
-  bumpScale: 0.01,
-});
-
-let mesh2 = new THREE.Mesh(geo2, material2);
-mesh2.position.set(3.2, 1.8, 0);
-scene.add(mesh2);
-
-// material geometry asteroid (atas)
-const material3 = new THREE.MeshPhongMaterial({
-  map: asteroid,
-  shininess: 500,
-  bumpMap: asteroid,
-  bumpScale: 0.01,
-});
-
-let mesh3 = new THREE.Mesh(geo3, material3);
-mesh3.position.set(-7, 2.7, -5);
-scene.add(mesh3);
-
-// material custom geometri asteroid (bawah)
-geo4.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-geo4.setAttribute("uv", new THREE.BufferAttribute(uvs, 2));
-
-let material4 = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  map: asteroid,
-});
-
-let mesh4 = new THREE.Mesh(geo4, material4);
-mesh4.position.set(-6.4, -3, -5.5);
-scene.add(mesh4);
 
 // fungsi animasi
 function update() {
-  mesh.rotation.y += 0.02;
-
-  mesh2.rotation.x += 0.02;
-  mesh2.rotation.y += 0.02;
-
-  mesh3.rotation.x += 0.01;
-  mesh3.rotation.y += 0.01;
-
-  mesh4.rotation.x += 0.03;
-  // mesh4.rotation.y += 0.03;
+  mesh.rotation.y += 0.01;
+  mesh.rotation.x += 0.01;
 
   requestAnimationFrame(update);
   renderer.render(scene, camera);
