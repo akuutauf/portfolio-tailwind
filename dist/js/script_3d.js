3635863388c1f6abf59dcdf791514808522b0726
akuutauf <taufikhidayat09121@gmail.com>
2022-05-21 20:31:06 +0700

    Add new Model 3D Objects

--- dist/js/script_3d.js
+++ dist/js/script_3d.js
@@ -10,6 +10,12 @@ let height = window.innerHeight;
 // scene
 const scene = new THREE.Scene();
 
+// scene background texture
+var loader = new THREE.TextureLoader();
+loader.load("https://i.postimg.cc/HLjxy4xT/5532919.jpg", function (texture) {
+  scene.background = texture;
+});
+
 // camera
 const camera = new THREE.PerspectiveCamera(55, width / height, 0.2, 10);
 camera.position.z = 3.5;
@@ -20,10 +26,13 @@ renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
 // Geometri Earth
-const geo = new THREE.SphereGeometry(1.1, 20, 20);
+const earthPlanet = new THREE.SphereGeometry(0.9, 20, 20);
+
+// Geometri Moon
+const moonPlanet = new THREE.SphereGeometry(0.4, 20, 20);
 
 // planet textures
-const neptuneFragment = new THREE.TextureLoader().load("https://i.postimg.cc/SsGh3m8T/neptune.jpg");
+const moon = new THREE.TextureLoader().load("https://i.postimg.cc/L6FXTf41/moon.jpg");
 const earth = new THREE.TextureLoader().load("https://i.postimg.cc/D0yTKRP2/earth.jpg");
 
 // MeshLambertMaterial
@@ -45,14 +54,36 @@ const material = new THREE.MeshPhongMaterial({
   bumpScale: 0.015,
 });
 
-let mesh = new THREE.Mesh(geo, material);
+let mesh = new THREE.Mesh(earthPlanet, material);
 mesh.position.set(0, 0, 0);
 scene.add(mesh);
+
+// material texture geometry moon
+const material2 = new THREE.MeshPhongMaterial({
+  map: moon,
+  shininess: 500,
+  bumpMap: moon,
+  bumpScale: 0.015,
+});
+
+let mesh2 = new THREE.Mesh(moonPlanet, material2);
+mesh2.position.set(1.9, 0.3, 0);
+scene.add(mesh2);
+
+// Fungsi interaksi object 3d
+document.addEventListener("mousemove", (interaction) => {
+  camera.position.x = (interaction.x - window.innerWidth / 1) * 0.002;
+  camera.position.y = (interaction.y - window.innerWidth / 2.5) * 0.002;
+  camera.lookAt(scene.position);
+});
 
 // fungsi animasi
 function update() {
   mesh.rotation.y += 0.01;
-  mesh.rotation.x += 0.01;
+  // mesh.rotation.x += 0.01;
+
+  mesh2.rotation.y += 0.009;
+  mesh2.rotation.x += 0.009;
 
   requestAnimationFrame(update);
   renderer.render(scene, camera);
